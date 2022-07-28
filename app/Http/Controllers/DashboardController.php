76e5390ef41f531842012353498930a4921e2915
master <tanvir.khan@penitt.com>
2022-07-28 20:42:34 +0600

    mane some controller and view function

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,21 +10,21 @@ class DashboardController extends Controller{
         return view('home/index');
     }
     public function all_role(){
-        $data['page_title'] = 'Admin | All Role';
+        $data['page_title'] = 'Role | All Role';
         $data['users'] = true;
         $data['all_role'] = true;
         return view('Role/all',$data);
     }
     //user list 
     public function all_user(){
-        $data['page_title'] = 'Admin | All User';
+        $data['page_title'] = 'User | All User';
         $data['users'] = true;
         $data['all_user'] = true;
         return view('users/allUser',$data);
     }
     //admin user list 
     public function all_admin_user(){
-        $data['page_title'] = 'Admin | All Admin User';
+        $data['page_title'] = 'User | All Admin User';
         $data['users'] = true;
         $data['all_admin_user'] = true;
         return view('users/allAdminUser',$data);
